2de99a27a249e947b5fe8eadaa54f0dd894013d3
PoetraAdy <33596070+PoetraAdy@users.noreply.github.com>
2020-02-24 18:56:39 +0700

    Core Game

---
+++ PulseMotion/VideoShaders/Demo/ChooseColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseColor : MonoBehaviour
+{
+    [SerializeField] Camera camera;
+
+    public void ChangeColorWall()
+    {
+        camera.backgroundColor = gameObject.GetComponent<Image>().color;
+    }
+}
